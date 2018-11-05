3373d8a9cb43dec58197c95857caaf55c73911f1
Vladlan <lanvlad911@gmail.com>
2018-11-05 10:53:56 +0000

    attached docker container to host directory

--- start.sh
+++ start.sh
@@ -9,4 +9,7 @@ docker build -t vladlan/node-web-app .
 #--rm		Automatically remove the container when it exits
 #-t tty - allocate a terminal so you can directly interact with the docker command
 #-i - interactive - connects STDIN to the allocated terminal. Any command you enter after this via your keyboard goes into the terminal
-docker run -d --name=node-web-app -p 49160:6069 --volumes-from vsftpd-docker vladlan/node-web-app
+#docker run -d --name=node-web-app -p 49160:6069 --volumes-from vsftpd-docker vladlan/node-web-app
+#docker run -dit --restart unless-stopped --name=vsftpd-docker -p 32021:21 -p 32022-32041:32022-32041 -v FtpVolume:/var/lib/ftp/incoming vsftpd-anonymous-upload-docker
+docker run -d --name=node-web-app -p 49160:6069 --mount src='/var/ftp/pub',target=/var/lib/ftp/incoming,type=bind  vladlan/node-web-app
+
